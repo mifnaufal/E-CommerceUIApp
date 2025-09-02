cea9b15d2ab45412768d39690955ea656296b17c
mifnaufal <naufalhhdd@gmail.com>
2025-09-02 01:28:24 +0000

    50%

---
+++ lib/home_page.dart
@@ -0,0 +1,113 @@
+// lib/home_page.dart
+
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'home_appbar.dart';
+import 'categories_widget.dart';
+import 'items_widget.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0; 
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          const HomeAppBar(),
+          Container(
+            padding: const EdgeInsets.only(top: 15),
+            decoration: const BoxDecoration(
+              color: Color(0xFFEDECF2),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(35),
+                topRight: Radius.circular(35),
+              ),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 15),
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Search here...",
+                          ),
+                        ),
+                      ),
+                      const Icon(
+                        Icons.camera_alt,
+                        size: 27,
+                        color: Color(0xFF4C53A5),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 20,
+                    horizontal: 10,
+                  ),
+                  child: const Text(
+                    "Categories",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF4C53A5),
+                    ),
+                  ),
+                ),
+                const CategoriesWidget(),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                  child: const Text(
+                    "Best Selling",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF4C53A5),
+                    ),
+                  ),
+                ),
+                const ItemsWidget(),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.transparent,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        height: 70,
+        color: const Color(0xFF4C53A5),
+        items: const [
+          Icon(Icons.home, size: 30, color: Colors.white),
+          Icon(Icons.shopping_cart, size: 30, color: Colors.white),
+          Icon(Icons.list, size: 30, color: Colors.white),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/items_widget.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+class ItemsWidget extends StatelessWidget {
+  const ItemsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      childAspectRatio: 0.68,
+      physics: const NeverScrollableScrollPhysics(),
+      crossAxisCount: 2,
+      shrinkWrap: true,
+      children: [
+        for (int i=1; i<8; i++)
+          Container(
+            padding: const EdgeInsets.only(left: 15, right: 15, top: 10),
+            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF4C53A5),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: const Text(
+                        "-50%",
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const Icon(
+                      Icons.favorite_border,
+                      color: Colors.red,
+                    )
+                  ],
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    margin: const EdgeInsets.all(10),
+                    child: Image.asset(
+                      "assets/images/carts/$i.png",
+                       height: 120,
+                       width: 120,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  alignment: Alignment.centerLeft,
+                  child: const Text(
+                    "Product Title",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Color(0xFF4C53A5),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: const Text(
+                    "Write description of product",
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Color(0xFF4C53A5),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "\$55",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF4C53A5)
+                        ),
+                      ),
+                       RatingBar.builder(
+                          initialRating: 4,
+                          minRating: 1,
+                          direction: Axis.horizontal,
+                          itemCount: 5,
+                          itemSize: 20,
+                          itemPadding: const EdgeInsets.symmetric(horizontal: 4),
+                          itemBuilder: (context, _) => const Icon(
+                            Icons.favorite,
+                            color: Color(0xFF4C53A5),
+                          ),
+                          onRatingUpdate: (index){},
+                        ),
+                      const Icon(
+                        Icons.shopping_cart_checkout,
+                        color: Color(0xFF4C53A5),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+      ],
+    );
+  }
+}
